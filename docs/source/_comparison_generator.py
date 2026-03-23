54470e2c75d0e0d1738efff8b74383c0ac38da7c
Matt Haberland <matt.haberland@gmail.com>
2026-03-22 21:33:39 -0700

    Fix formatting in _comparison_generator.py

--- docs/source/_comparison_generator.py
+++ docs/source/_comparison_generator.py
@@ -219,6 +219,7 @@ def generate():
         'An alternative is `MArray <https://github.com/mdhaber/marray>`__, '
         'which adds mask support to Python Array API Standard-compatible '
         'array backends, including CuPy.'
+        ''
     ]
     buf += _section(
         'Linear Algebra',
